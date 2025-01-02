2b5789d15af21ae7834719f8358f543a1926e5ab
Temct <150273475+Defferentior@users.noreply.github.com>
2025-01-02 09:16:35 -0800

    Add readme, and simple spans to the li

--- code.js
+++ code.js
@@ -2,7 +2,13 @@ function McTerialLoad() {
   const elementList = document.querySelector('.Periodic_Element_List');
   for (let i = 0; i < PeriodicElements.length; i++) {
     let li = document.createElement('li');
-    li.textContent = PeriodicElements[i].name;
+    li.className = `Element-${PeriodicElements[i].symbol}`;
+    li.innerHTML = `
+      <span class="position">${PeriodicElements[i].position}</span>
+      <span class="symbol">${PeriodicElements[i].symbol}</span>
+      <span class="name">${PeriodicElements[i].name}</span>
+      <span class="weight">${PeriodicElements[i].weight}</span>
+    `;
     elementList.appendChild(li);
   }}
 
---
+++ readme.txt
@@ -0,0 +1,5 @@
+# McTerial JS
+
+This is a not too serious sandbox project that is for the purpose of practicing with simple vanilla javascript
+
+It involves very bare minimum work, and will not be updated often, as I am more focused on other projects.
