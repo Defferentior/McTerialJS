6bcb8aa12df547d5da9d77b0e3cba3f4dcca6864
Temct <150273475+Defferentior@users.noreply.github.com>
2025-01-02 09:31:12 -0800

    Added more to the list

--- list.js
+++ list.js
@@ -13,3 +13,14 @@ PeriodicElements.push({position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'
 PeriodicElements.push({position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'});
 PeriodicElements.push({position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'});
 PeriodicElements.push({position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'});
+PeriodicElements.push({position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'});
+PeriodicElements.push({position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'});
+PeriodicElements.push({position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'});
+PeriodicElements.push({position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'});
+PeriodicElements.push({position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'});
+PeriodicElements.push({position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'});
+PeriodicElements.push({position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'});
+PeriodicElements.push({position: 21, name: 'Scandium', weight: 44.9559, symbol: 'Sc'});
+PeriodicElements.push({position: 22, name: 'Titanium', weight: 47.867, symbol: 'Ti'});
+PeriodicElements.push({position: 23, name: 'Vanadium', weight: 50.9415, symbol: 'V'});
+PeriodicElements.push({position: 24, name: 'Chromium', weight: 51.9961, symbol: 'Cr'});
